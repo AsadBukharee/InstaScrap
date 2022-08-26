75f950327754c75779ce7c9f92fec416f4b825a3
Asad Bukhari <asadbukharee@gmail.com>
2022-08-26 19:54:35 +0500

    requirements frozen

---
+++ app/utills/youtube_video_uploader.py
@@ -0,0 +1,33 @@
+from simple_youtube_api.Channel import Channel
+from simple_youtube_api.LocalVideo import LocalVideo
+
+# loggin into the channel
+channel = Channel()
+channel.login("client_secret.json", "credentials.storage")
+
+# setting up the video that is going to be uploaded
+video = LocalVideo(file_path="test_vid.mp4")
+
+# setting snippet
+video.set_title("My Title")
+video.set_description("This is a description")
+video.set_tags(["this", "tag"])
+video.set_category("gaming")
+video.set_default_language("en-US")
+
+# setting status
+video.set_embeddable(True)
+video.set_license("creativeCommon")
+video.set_privacy_status("private")
+video.set_public_stats_viewable(True)
+
+# setting thumbnail
+video.set_thumbnail_path('test_thumb.png')
+
+# uploading video and printing the results
+video = channel.upload_video(video)
+print(video.id)
+print(video)
+
+# liking video
+video.like()
